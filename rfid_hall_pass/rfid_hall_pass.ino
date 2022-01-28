401de955071d22572346c60f297793203fed1bcb
ecuber <37519236+ecuber@users.noreply.github.com>
2022-01-28 14:10:42 -0500

    Debugging and setup for PCB

--- rfid_hall_pass/rfid_hall_pass.ino
+++ rfid_hall_pass/rfid_hall_pass.ino
@@ -12,8 +12,10 @@
 #include <RTClib.h>
 RTC_DS1307 rtc;
 #include "SoftwareSerial.h"
-#define TX_PIN 6 // Arduino transmit  YELLOW WIRE  labeled RX on printer
-#define RX_PIN 5 // Arduino receive   GREEN WIRE   labeled TX on printer
+#define TX_PIN 5 // Arduino transmit  YELLOW WIRE  labeled RX on printer
+#define RX_PIN 4 // Arduino receive   GREEN WIRE   labeled TX on printer
+
+#define LED_PIN 69 // need to wire the LED to a digital pin not the 5V
 
 #include <SPI.h>
 #include <MFRC522.h>
@@ -35,6 +37,7 @@ Adafruit_Thermal printer(&mySerial);     // Pass addr to printer constructor
 
 void setup() {  
   Serial.begin(9600);
+  Serial.println("we're online");
   SPI.begin(); // Init SPI bus
   rfid.PCD_Init(); // Init MFRC522
 
@@ -48,7 +51,7 @@ void setup() {
     Serial.println("Couldn't find RTC");
   }
   rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
-  
+  Serial.println("RTC updated");
 }
 
 void loop() {
@@ -66,12 +69,6 @@ void loop() {
   for (byte i = 0; i < 4; i++) {
     nuidPICC[i] = rfid.uid.uidByte[i];
   }
-  
-  DateTime now = rtc.now();
-  int pm = now.hour() >= 12;
-  int hourF = pm ? now.hour() - 12 : now.hour();
-  int mins = now.minute();
-  int secs = now.second();
 
   // printer
   pinMode(7, OUTPUT); digitalWrite(7, LOW);
@@ -82,10 +79,20 @@ void loop() {
     userid += String(rfid.uid.uidByte[i], HEX);
   }
 
-  Serial.println(lastUID);
-  Serial.println(userid);
+//  Serial.println(lastUID);
+//  Serial.println(userid);
+//  Serial.print(hourF);
+//  Serial.print(':');
+//  Serial.print(mins);
+//  Serial.println();
 
   if (!lastUID.equals(userid)) {
+    DateTime now = rtc.now();
+    int pm = now.hour() >= 12;
+    int hourF = pm ? now.hour() - 12 : now.hour();
+    int mins = now.minute();
+    int secs = now.second();
+    
     printer.begin();
     printer.printBitmap(hallpass_width, hallpass_height, hallpass_data);
     printer.feed(1);
@@ -108,6 +115,9 @@ void loop() {
     printer.boldOn();
     printer.print(hourF);
     printer.print(':');
+    if (mins < 10) {
+      printer.print("0");
+    }
     printer.print(mins);
     printer.println(pm ? " pm" : " am");
     printer.boldOff();
